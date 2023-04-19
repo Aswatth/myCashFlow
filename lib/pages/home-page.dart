d59b06e1403bdbe66efaa0c28589a0a5a3b0e1d7
Aswatth <aswatth56@gmail.com>
2023-04-19 07:36:11 +0530

    Update home-page.dart

--- lib/pages/home-page.dart
+++ lib/pages/home-page.dart
@@ -1,10 +1,13 @@
+import 'dart:convert';
+
 import 'package:flutter/material.dart';
+import 'package:my_cash_flow/models/account-model.dart';
 import 'package:my_cash_flow/pages/budget-page.dart';
 import 'package:my_cash_flow/pages/profile-page.dart';
 import 'package:my_cash_flow/pages/transaction-page.dart';
 
 class HomePage extends StatefulWidget {
-  const HomePage({Key? key}) : super(key: key);
+  HomePage({Key? key}) : super(key: key);
 
   @override
   _HomePageState createState() => _HomePageState();
@@ -14,12 +17,21 @@ class _HomePageState extends State<HomePage> {
 
   int _selectedIndex = 0;
 
-  List<Widget> _widgets = <Widget>[
-    Center(child: Text("Home page"),),
-    TransactionPage(),
-    BudgetPage(),
-    ProfilePage()
-  ];
+  List<Widget> _widgets = [];
+
+  @override
+  void initState() {
+    super.initState();
+
+    //print(jsonEncode(widget.accountModel.toJson()));
+
+    _widgets = <Widget>[
+      Center(child: Text("This is a home page ")),
+      TransactionPage(),
+      BudgetPage(),
+      ProfilePage()
+    ];
+  }
 
   @override
   Widget build(BuildContext context) {
