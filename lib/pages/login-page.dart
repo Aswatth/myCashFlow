10caf16b87bda31a3d4d2c3fa00e5b25243e6ef4
Aswatth <aswatth56@gmail.com>
2023-05-17 06:50:51 +0530

    Removed duplicate code

--- lib/pages/login-page.dart
+++ lib/pages/login-page.dart
@@ -62,158 +62,110 @@ class _LoginState extends State<Login> {
   int validationResult = -1;
   String errorMessage = "";
 
+  nextSteps(String password){
+    if(!widget.isNewUser){
+      //Validate password
+      validatePassword(password).then((value){
+        if(value){
+          //Navigate to homepage
+          Navigator.push(
+            context,
+            MaterialPageRoute(
+              builder: (context) => BasePage(pageIndex: 0,),
+            ),
+          );
+        }
+        else{
+          setState(() {
+            errorMessage = "Incorrect password";
+          });
+        }
+      });
+    }
+    else{
+      //Save password
+      savePassword(password);
+
+      //Navigate to account creation page
+      //Clearing navigation stack
+      Navigator.pushAndRemoveUntil(
+          context,
+          MaterialPageRoute(
+            builder: (context) => AccountCreationPage(
+              isNewUser: widget.isNewUser,
+            ),),
+              (route) => false);
+    }
+  }
+
   Future<bool> validatePassword(password) async {
     return await PasswordDbHelper.instance.validatePassword(password);
-    /*setState(() {
-      validationResult = result?1:0;
-    });
-    return result;*/
   }
 
   savePassword(String password) async {
     await PasswordDbHelper.instance.setPassword(password);
   }
 
-  createWidget() {
-    //If existing user then display widget to login using their password
-    if (!widget.isNewUser) {
-      return Column(
-        mainAxisAlignment: MainAxisAlignment.center,
-        crossAxisAlignment: CrossAxisAlignment.start,
-        children: [
-          Form(
-            key: _formKey,
-            child: TextFormField(
-              obscureText: true,
-              autocorrect: false,
-              enableSuggestions: false,
-              decoration: const InputDecoration(
-                hintText: "Enter your password",
-                prefixIcon: Icon(Icons.key, color: Color(0xFF1C2536),),
-                enabledBorder: OutlineInputBorder(
-                  borderRadius: BorderRadius.all(Radius.circular(20.0)),
-                  borderSide: BorderSide(
-                    color: Color(0xFF1C2536),
-                  ),
-                ),
-                focusedBorder: OutlineInputBorder(
-                  borderRadius: BorderRadius.all(Radius.circular(20.0)),
-                  borderSide: BorderSide(
-                    color: Color(0xFF1C2536),
-                  ),
-                ),
-              ),
-              validator: (value) {
-                if (value == null || value.isEmpty) {
-                  return "Please enter password";
-                }
-                return null;
-              },
-              onSaved: (value) {
-                validatePassword(value).then((value){
-                  if(value){
-                    Navigator.push(
-                      context,
-                      MaterialPageRoute(
-                        builder: (context) => BasePage(pageIndex: 0,),
-                      ),
-                    );
-                  }
-                  else{
-                    setState(() {
-                      errorMessage = "Incorrect password";
-                    });
-                  }
-                });
-              },
-            ),
-          ),
-          errorMessage.isNotEmpty?Text(errorMessage):Container(),
-          Container(
-            width: double.infinity,
-            decoration: BoxDecoration(
-                color: const Color(0xFF1C2536),
-                borderRadius: BorderRadius.circular(20.0)),
-            child: TextButton(
-              child: const Text("Log in", style: TextStyle(color: Colors.white),),
-              onPressed: () {
-                _formKey.currentState!.save();
-              },
-            ),
-          )
-        ].map((e) => Padding(padding: const EdgeInsets.all(10.0),child: e,)).toList(),
-      );
-    }
-    //If not then set up password for new user
-    else {
-      return Column(
-        mainAxisAlignment: MainAxisAlignment.center,
-        children: [
-          Form(
-            key: _formKey,
-            child: TextFormField(
-              obscureText: true,
-              autocorrect: false,
-              enableSuggestions: false,
-              decoration: const InputDecoration(
-                hintText: "Password",
-                prefixIcon: Icon(Icons.key, color: Color(0xFF1C2536)),
-                enabledBorder: OutlineInputBorder(
-                  borderRadius: BorderRadius.all(Radius.circular(20.0)),
-                  borderSide: BorderSide(
-                    color: Color(0xFF1C2536),
-                  ),
-                ),
-                focusedBorder: OutlineInputBorder(
-                  borderRadius: BorderRadius.all(Radius.circular(20.0)),
-                  borderSide: BorderSide(
-                    color: Color(0xFF1C2536),
-                  ),
-                ),
-              ),
-              validator: (value) {
-                if (value == null || value.isEmpty) {
-                  return "Please enter password";
-                } else {
-                  return null;
-                }
-              },
-              onSaved: (_) {
-                if (_formKey.currentState!.validate()) {
-                  //Saving the password
-                  savePassword(_!);
-
-                  //Clearing navigation stack
-                  Navigator.pushAndRemoveUntil(
-                      context,
-                      MaterialPageRoute(
-                          builder: (context) => AccountCreationPage(
-                                isNewUser: widget.isNewUser,
-                              ),),
-                      (route) => false);
-                }
-              },
-            ),
-          ),
-          Container(
-            width: double.infinity,
-            decoration: BoxDecoration(
-                color: const Color(0xFF1C2536),
-                borderRadius: BorderRadius.circular(20.0)),
-            child: TextButton(
-              child: const Text("Setup password", style: TextStyle(color: Colors.white),),
-              onPressed: () {
-                _formKey.currentState!.save();
-              },
-            ),
-          )
-        ].map((e) => Padding(padding: EdgeInsets.all(10.0),child: e,)).toList(),
-      );
-    }
-  }
-
   @override
   Widget build(BuildContext context) {
-    return createWidget();
+    return Column(
+      mainAxisAlignment: MainAxisAlignment.center,
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        Form(
+          key: _formKey,
+          child: TextFormField(
+            obscureText: true,
+            autocorrect: false,
+            maxLength: 16,
+            enableSuggestions: false,
+            decoration: const InputDecoration(
+              hintText: "Enter your password",
+              hintMaxLines: 16,
+              prefixIcon: Icon(Icons.key, color: Color(0xFF1C2536),),
+              enabledBorder: OutlineInputBorder(
+                borderRadius: BorderRadius.all(Radius.circular(20.0)),
+                borderSide: BorderSide(
+                  color: Color(0xFF1C2536),
+                ),
+              ),
+              focusedBorder: OutlineInputBorder(
+                borderRadius: BorderRadius.all(Radius.circular(20.0)),
+                borderSide: BorderSide(
+                  color: Color(0xFF1C2536),
+                ),
+              ),
+            ),
+            validator: (value) {
+              if (value == null || value.isEmpty) {
+                return "Please enter password";
+              }
+              if(value.length <4){
+                return "Password should have at least 4 characters";
+              }
+              return null;
+            },
+            onSaved: (value) {
+              if(_formKey.currentState!.validate()) {
+                nextSteps(value!);
+              }
+            },
+          ),
+        ),
+        errorMessage.isNotEmpty?Text(errorMessage):Container(),
+        Container(
+          width: double.infinity,
+          decoration: BoxDecoration(
+              color: const Color(0xFF1C2536),
+              borderRadius: BorderRadius.circular(20.0)),
+          child: TextButton(
+            child: const Text("Log in", style: TextStyle(color: Colors.white),),
+            onPressed: () {
+              _formKey.currentState!.save();
+            },
+          ),
+        )
+      ].map((e) => Padding(padding: const EdgeInsets.all(10.0),child: e,)).toList(),
+    );
   }
 }
