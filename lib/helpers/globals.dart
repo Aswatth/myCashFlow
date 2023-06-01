e383709a1250f6e195a9b579c3e3b8dfcf0c2a02
Aswatth <aswatth56@gmail.com>
2023-06-01 17:27:48 +0530

    Removed months from global

--- lib/helpers/globals.dart
+++ lib/helpers/globals.dart
@@ -1,19 +1,4 @@
 import 'package:intl/intl.dart';
 
-final List<String> Months = [
-  "Jan",
-  "Feb",
-  "Mar",
-  "Apr",
-  "May",
-  "Jun",
-  "Jul",
-  "Aug",
-  "Sep",
-  "Oct",
-  "Nov",
-  "Dec"
-];
-
 NumberFormat NumberFormatter = NumberFormat("##,##,###");
 DateFormat DateFormatter = DateFormat("dd-MMM-yyyy");
