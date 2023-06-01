6ebb36c3aca0d8906c12dc456e6fd84b6eeb7cff
Aswatth <aswatth56@gmail.com>
2023-06-01 16:32:12 +0530

    Adding personal and household categories

--- lib/models/transaction-category.dart
+++ lib/models/transaction-category.dart
@@ -9,6 +9,9 @@ class Category{
 }
 
 final List<Category> categoryList = [
+  Category("Salary", Icons.attach_money, Colors.lime),
+  Category("Personal", Icons.person, Colors.red),
+  Category("House-hold", Icons.home, Colors.yellow),
   Category("Family", Icons.family_restroom, Colors.lightGreenAccent),
   Category("Friends", Icons.group, Colors.orangeAccent),
   Category("Medical", Icons.healing, Colors.yellowAccent),
@@ -17,6 +20,5 @@ final List<Category> categoryList = [
   Category("Education", Icons.school, Colors.cyanAccent),
   Category("Entertainment", Icons.theater_comedy, Colors.redAccent),
   Category("Travel", Icons.airplanemode_active, Colors.pinkAccent),
-  Category("Salary", Icons.attach_money, Colors.lime),
   Category("Other", Icons.shuffle, Colors.green),
 ];
