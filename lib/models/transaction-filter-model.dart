8e40a232d20517489665f1923155de9bbffb25b2
Aswatth <aswatth56@gmail.com>
2023-06-01 17:28:00 +0530

    Removed unused imports

--- lib/models/transaction-filter-model.dart
+++ lib/models/transaction-filter-model.dart
@@ -1,8 +1,4 @@
-import 'package:my_cash_flow/models/transaction-model.dart';
-import 'package:my_cash_flow/models/transactionTypeEnum.dart';
-
 import '../helpers/enums.dart';
-import 'transaction-category.dart';
 
 class TransactionFilterModel{
   DateTime? startDate;
